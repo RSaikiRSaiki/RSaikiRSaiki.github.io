bc5940354c9c6c2863b1c072ae31f49fc56719a3
RSaiki <saikiryunosuke@gmail.com>
2021-06-04 02:47:02 +0900

    test

---
+++ js/jsmail.js
@@ -0,0 +1,39 @@
+function secretaddress() {
+	var ma = String.fromCharCode(112-5,107+14,111-0,108+8,142-31,24+22,133-24,95+6,117-17,111+5,129-28,74+23,60-10,28+20,72-23,27+30,91-27,79+24,109-0,67+30,122-17,106+2,58-12,78+21,113-2,104+5);
+	var mt = String.fromCharCode(147-38,69+28,146-41,52+56,143-27,61+50,105-47);
+	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
+}
+
+
+// http://www.nishishi.com/scripts/jsmail/
+//件名→行きたい新歓
+//お問い合わせありがとうございます！件名に行きたい新歓を、以下の本文に学部学科、お名前、その他連絡事項をご記入お願いします！
+//※ PCメールを受信拒否されている場合、こちらからの返信が届かない場合があります。
+//
+// 学部・学科：
+// お名前：
+// 連絡事項：
+//
+// 部員一同、お待ちしております♪
+
+//http://www.tagindex.com/tool/url.html
+//ここで変換
+
+
+function secretaddress2() {
+	var ma = String.fromCharCode(107,121,111,116,111,46,109,101,100,116,101,97,51,48,64,103,109,97,105,108,46,99,111,109);
+	var mt = String.fromCharCode(109,97,105,108,116,111,58);
+	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
+}
+function secretaddress3() {
+	var ma = String.fromCharCode(107,121,111,116,111,46,109,101,100,105,99,97,108,46,116,101,97,64,103,109,97,105,108,46,99,111,109);
+	var mt = String.fromCharCode(109,97,105,108,116,111,58);
+	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
+}
+
+//2021年度新歓用
+function secretaddress_2021() {
+	var ma = String.fromCharCode(107,121,111,116,111,46,109,101,100,116,101,97,50,48,50,49,64,103,109,97,105,108,46,99,111,109);
+	var mt = String.fromCharCode(109,97,105,108,116,111,58);
+	document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
+}
